b2b8a6ec71e7a0716c22c032edf89dc60ea43d7b
Almirio Silva <almirio.bsb@gmail.com>
2022-05-11 19:53:44 -0300

    aula 14 runner e steps

---
+++ src/test/java/br/com/chronosAcademy/runner/RunnerTest.java
@@ -0,0 +1,16 @@
+package br.com.chronosAcademy.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = "@primeiro",
+        glue = "src/test/br/com/chronosAcademy/steps/",
+        features = "src/test/resources/features/",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = {"json:target/reports/CucumberReports.json", "pretty"}
+)
+public class RunnerTest {
+}
